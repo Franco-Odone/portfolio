f2871344a1668626aaf6dfc7c86e124d26ac47c3
Franco Odone <108704414+Franco-Odone@users.noreply.github.com>
2023-02-10 22:39:15 -0300

    se avanzo en el componente de contacto

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,103 @@
+import { useEffect, useRef, useState } from 'react';
+import { AnimatedLetters } from '../AnimatedLetters/AnimatedLetters';
+
+import emailjs from '@emailjs/browser';
+
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 import './contact.scss';
 
 const Contact = () => {
-  return 'Hello';
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const form = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(() => {
+        alert('Message successfully sent!');
+        window.location.reload(false);
+      })
+      .catch(() => {
+        alert('Failed to send the message, please try again');
+      });
+  };
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={'Contact me'.split('')}
+              index={1}
+              letterClass={letterClass}
+            />
+          </h1>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <button type="submit" className="send-button">
+                    Send
+                  </button>
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[-33.45694, -70.64827]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[-33.45694, -70.64827]}>
+              <Popup>
+                {'Franco lives here, come over for a cup of coffee :)'}
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export { Contact };
